8f878949d808a555aa6889933f9d2a989e0ec20a
Ashish Kumar <ashish0981kumar@gmail.com>
2025-04-03 13:10:59 +0530

    Update Customer_prchase_analysis.py
    Task:
    
    1. Find total annual sales.
    2. Best performing region.
    3. Worst performing region.
    4. Best quarter.
    5. Worst quarter.

--- Customer_prchase_analysis.py
+++ Customer_prchase_analysis.py
@@ -1,24 +1,26 @@
-customer_purchases = {
-    "Alice": {"Groceries": 120, "Electronics": 300, "Clothing": 150},
-    "Bob": {"Groceries": 80, "Electronics": 500, "Clothing": 200},
-    "Charlie": {"Groceries": 100, "Electronics": 250, "Clothing": 180}
+region_sales = {
+    "North": {"Q1": 15000, "Q2": 18000, "Q3": 22000, "Q4": 25000},
+    "South": {"Q1": 17000, "Q2": 19000, "Q3": 21000, "Q4": 23000},
+    "East": {"Q1": 20000, "Q2": 22000, "Q3": 25000, "Q4": 27000}
 }
-purchases = {}
-customer_spending = {}
-product_totals = {"Groceries": 0, "Electronics": 0, "Clothing": 0}
-category = {}
-for name, products in customer_purchases.items():
-    total_spending = sum(products.values())
-    customer_spending[name] = total_spending
+total_region_sales = {}
+quarterly_sales = {"Q1": 0, "Q2": 0, "Q3": 0, "Q4": 0}
+for region, quarters in region_sales.items():
+    total_region_sales[region] = sum(quarters.values())
+    for quarter, sales in quarters.items():
+        quarterly_sales[quarter] += sales
 
-    print(f"Total spending of {name}: {total_spending}")
-    for product, quantity in products.items():
-        #print(product)
-        product_totals[product]+= quantity
+    #print(f"Total annual sales for {region} : {sum(quarter.values())}")
+#print(total_region_sales)
 
+print("Total annual sales per region: \n")
+for region, total in total_region_sales.items():
+    print(f"{region} : ${total}")
 
-highest_customer_spending = max(customer_spending, key=customer_spending.get)
-print(f"Highest Customer Spending : {highest_customer_spending}")
-#print(product_totals)
-highest_total_sales = max(product_totals, key=product_totals.get)
-print(f"Category with the highest total sales: {highest_total_sales}" )
+print(f"\nBest performing region: {max(total_region_sales, key=total_region_sales.get)}")
+print(f"Worst performing region: {min(total_region_sales, key=total_region_sales.get)}")
+print(f"\nBest Quarter: {max(quarterly_sales, key=quarterly_sales.get)}")
+print(f"Worst Quarter: {min(quarterly_sales,key=quarterly_sales.get)}")
+#print(total_region_sales)
+#print(quarterly_sales)
+
