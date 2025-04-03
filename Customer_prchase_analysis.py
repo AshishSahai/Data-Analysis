71a7c37d8944cbc804a01c6af51f8d50effcf0d7
Ashish Kumar <ashish0981kumar@gmail.com>
2025-04-03 12:39:05 +0530

    Update Customer_prchase_analysis.py
    Tasks:
    1. Total spending per customer.
    2. Highest spending customer.
    3. Total sales per category.
    4. Category with the highest total sales.

--- Customer_prchase_analysis.py
+++ Customer_prchase_analysis.py
@@ -4,15 +4,21 @@ customer_purchases = {
     "Charlie": {"Groceries": 100, "Electronics": 250, "Clothing": 180}
 }
 purchases = {}
+customer_spending = {}
 product_totals = {"Groceries": 0, "Electronics": 0, "Clothing": 0}
 category = {}
 for name, products in customer_purchases.items():
     total_spending = sum(products.values())
+    customer_spending[name] = total_spending
+
     print(f"Total spending of {name}: {total_spending}")
     for product, quantity in products.items():
         #print(product)
         product_totals[product]+= quantity
 
+
+highest_customer_spending = max(customer_spending, key=customer_spending.get)
+print(f"Highest Customer Spending : {highest_customer_spending}")
 #print(product_totals)
 highest_total_sales = max(product_totals, key=product_totals.get)
 print(f"Category with the highest total sales: {highest_total_sales}" )
